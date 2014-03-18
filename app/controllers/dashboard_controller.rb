7447136012f1ec0901dd8a35eec67db8cbb5f984
tomaszb <tbacewicz@gmail.com>
2014-03-17 23:32:01 -0700

    top users/bots chart working in detailed stats

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -27,11 +27,31 @@ end
 def details
 	params[:id] = session[:id] if params[:id].nil?
 	@user = User.find_by_authentication_id(params[:id])
+
+	puts params[:id]
+
+	usersorbots = params[:class_botuser]
+	type = (2 if usersorbots == "bot") || 1
+	@topips = Topip.where("company_id = ? AND type_id = ?", params[:id], type).sort_by{|e| -e[:request]}
+	
+	@topip_str = ""
+	@pages = Hash.new
+	i = 1
+	@topips.each do |ip|
+		@topip_str += '[' + i.to_s + ',' + ip.request.to_s + '],'
+		i += 1
+
+		@pages[ip.ipaddress] = ip.pages.all.sort_by{|e| -e[:freq]}
+	end
+
+
+
+
 	@requesttypes = Requesttype.where("company_id = ?", params[:id]).sort{|x,y| x.type_id <=> y.type_id }
 	@iptypes = Iptype.where("company_id = ?", params[:id]).sort{|x,y| x.type_id <=> y.type_id }
 	@dailyrequests = Dailyrequest.where("company_id = ?", params[:id]).sort{|x,y| x.type_id <=> y.type_id }
 	@hourrequests = Hourrequest.where("company_id = ?", params[:id]).sort{|x,y| x.type_id <=> y.type_id }
-	@topips = Topip.where("company_id = ?", params[:id]).sort{|x,y| y.request <=> x.request }
+	#@topips = Topip.where("company_id = ?", params[:id]).sort{|x,y| y.request <=> x.request }
 end
 
 def demo
