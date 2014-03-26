99e0eef348bebb92e4b8a1a316ade24639f89446
tomaszb <tbacewicz@gmail.com>
2014-03-25 17:26:45 -0700

    with blocking implemented

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+
 ActiveRecord::Schema.define(version: 20140325235928) do
 
   create_table "apiattrs", force: true do |t|
